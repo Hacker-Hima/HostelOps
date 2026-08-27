da432f4a3c190e3833c45fae49239098dbdd4f9e
Hacker-Hima <himapc2007@gmail.com>
2026-08-27 22:57:19 +0530

    frontend and Security

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ticketReducer from './ticketSlice';
-
-export const store = configureStore({
-  reducer: {
-    ticketStore: ticketReducer,
-  },
-});
